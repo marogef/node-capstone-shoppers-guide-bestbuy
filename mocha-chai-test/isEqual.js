d2f459264d8ead0c7aa95f92b0baa72c0a011a78
Faras Maroge <goldencrane33@yahoo.com>
2017-02-02 22:26:53 +0000

    Added more mocha test features, installed --save-dev mocha, mkdir test, touch test, added script in package.json, added require for const isEqual and used describe.

---
+++ mocha-chai-test/isEqual.js
@@ -0,0 +1,7 @@
+module.exports = function(a, b) {return a === b}
+const isEqual = require('../isEqual');
+
+describe('isEqual', function() {
+    it('should give right answers for equal and unequal inputs');
+  }
+);
