1c2044381962db74f4d1316cb2b290336391594b
Faras Maroge <goldencrane33@yahoo.com>
2017-02-03 16:33:21 +0000

    Fixed side-menu features

--- testItems.js
+++
@@ -1,9 +0,0 @@
-function testItems(a, b) {
-  // validate that both inputs are numbers
-  if (!(typeof a === 'number' && typeof b === 'number')) {
-    throw Error('Oh no!');
-  }
-  return a + b;
-}
-
-module.exports = testItems;
