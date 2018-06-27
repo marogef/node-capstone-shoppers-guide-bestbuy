ea6d6a87e8807737c4baaef397d1f95bcba15068
Faras Maroge <goldencrane33@yahoo.com>
2018-06-26 23:09:47 -0400

    Recovered local repository

--- models/product.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var ProductSchema = new mongoose.Schema({
-    name: { type: String, required: true }
-});
-
-var Product = mongoose.model('Product', ProductSchema);
-
-module.exports = Product;
